15dfab049be90f50e14ea25fd9311a750ba2a3f5
Fredy Clavijo <john.clavijo@globant.com>
2024-05-06 23:18:01 -0500

    fix

--- src/test/java/CucumberTestSuite.java
+++ src/test/java/CucumberTestSuite.java
@@ -1,5 +1,3 @@
-package starter;
-
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.IncludeTags;
@@ -11,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features/machpay")
-@IncludeTags("verifyBipCharge")
+//@IncludeTags("verifyBipCharge")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
 public class CucumberTestSuite {
 }
--- src/test/java/starter/stepdefinitions/EmployeeDef.java
+++ src/test/java/starter/stepdefinitions/EmployeeDef.java
@@ -1,4 +1,4 @@
-package com.poc.cl.definitions;
+package starter.stepdefinitions;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
